b5b12c0279498f0d6d8d29be3102f29d0d127dd7
koffiaxelsaintjeank-bit <koffiaxelsaintjeank@gmail.com>
2025-08-20 19:57:31 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Fj9STB5b#l6l9k3oO0hNMFxA8iEt_AE8uQXIPK3Y22SETHnaEHvs",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2250715405077", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
