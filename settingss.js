4018f41d5676fa76ad857c9f1453c835e81f0f24
koffiaxelsaintjeank-bit <koffiaxelsaintjeank@gmail.com>
2025-08-21 11:45:13 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Fj9STB5b#l6l9k3oO0hNMFxA8iEt_AE8uQXIPK3Y22SETHnaEHvs",  // add your Session Id here
+  SESSION_ID: "
   
-  OWNER_NUMBER: "2250715405077", // put your phone number here
+  OWNER_NUMBER: "", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
